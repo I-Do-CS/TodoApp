1cc93078b8f1c0efef0c77e3fd5c54414d2cb8f9
Ali Ghelichkhani <idocs4aliving@gmail.com>
2025-11-25 21:57:26 +0330

    Add db migration for soft delete support

---
+++ TodoApp.API/Migrations/20251125182626_Add_SoftDeleteForApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_SoftDeleteForApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+        }
+    }
+}
